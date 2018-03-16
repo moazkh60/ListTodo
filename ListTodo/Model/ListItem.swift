8e86fa984e4de3c7d482b634ae2410ad95e353b3
Muhammad Moaz Khan <moazkh60@gmail.com>
2018-03-16 21:35:12 +0500

    Code arranged into model, view and controller. ListItem model added.

---
+++ ListTodo/Model/ListItem.swift
@@ -0,0 +1,13 @@
+//
+//  ListItem.swift
+//  ListTodo
+//
+//  Created by Muhammad Moaz Khan on 16/03/2018.
+//  Copyright © 2018 MK. All rights reserved.
+//
+
+import Foundation
+
+class ListItem{
+    
+}
