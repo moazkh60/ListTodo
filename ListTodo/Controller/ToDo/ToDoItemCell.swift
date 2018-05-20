de591fa1431ce80a9942366390ae4082e7a80135
Muhammad Moaz Khan <moazkh60@gmail.com>
2018-05-20 11:20:22 +0500

    Todo List screen fetch data complete

---
+++ ListTodo/Controller/ToDo/ToDoItemCell.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoItemCell.swift
+//  ListTodo
+//
+//  Created by Muhammad Moaz Khan on 20/05/2018.
+//  Copyright © 2018 MK. All rights reserved.
+//
+
+import UIKit
+
+class ToDoItemCell: UITableViewCell {
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var title: UILabel!
+}
--- ListTodo/Controller/ToDo/TodoVC.swift
+++ ListTodo/Controller/ToDo/TodoVC.swift
@@ -7,14 +7,24 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoVC: UITableViewController {
     
     // Items list to store todos
     var todoList: [ToDo] = []
+    
+    // Core data context
+    var context: NSManagedObjectContext?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // context from app delegate singleton
+        context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
+        
+        // load todo list data
+        loadData()
     }
     
     // When add button on top is pressed
@@ -24,11 +34,9 @@ class TodoVC: UITableViewController {
         let alert = UIAlertController(title: "Add Todo Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Items", style: .default) { (action) in
             
-            // Context from app delegate singleton
-            let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
 
             // Context can't be nil
-            if let context = context{
+            if let context = self.context{
                 let todo = ToDo(context: context)
                 let title = itemTextField.text
                 
@@ -38,7 +46,8 @@ class TodoVC: UITableViewController {
                     self.todoList.append(todo)
                     
                     do{
-                     try context.save()
+                        try context.save()
+                        self.tableView.reloadData()
                     }catch{
                         fatalError("Error storing data")
                     }
@@ -55,8 +64,20 @@ class TodoVC: UITableViewController {
         present(alert, animated: true, completion: nil)
     }
     
+    // Load data from  coredata
+    func loadData(){
+        let request:NSFetchRequest<ToDo> = ToDo.fetchRequest()
+        do{
+            if let context = context{
+                todoList = try context.fetch(request)
+                tableView.reloadData()
+            }
+        }catch{
+            print("Error fetching data")
+        }
+    }
+    
     // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
@@ -65,57 +86,22 @@ class TodoVC: UITableViewController {
         return todoList.count
     }
 
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ToDoItemCell
+        cell.title.text = todoList[indexPath.item].title
         
         return cell
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "detailSegueId"{
+            let detailVC = segue.destination as? DetailVC
+            if let detailVC = detailVC{
+                //detailVC.text = todoList.text
+            }
+        }
+ 
     }
-    */
-
 }
