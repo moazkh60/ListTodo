d6ae8fa05c875a208d06baef24785bbaca649c15
Muhammad Moaz Khan <moazkh60@gmail.com>
2018-05-20 11:24:36 +0500

    Detail view controller set

--- ListTodo/Controller/DetailVC.swift
+++ ListTodo/Controller/DetailVC.swift
@@ -10,26 +10,13 @@ import UIKit
 
 class DetailVC: UIViewController {
 
+    @IBOutlet weak var detailTextView: UITextView!
+    var text: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //set the detail text
+        detailTextView.text = text
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
